281854cac9b917b089de714bf719e4a7f484e487
Francisco Rangel <francisco.rangel@s2g.energy>
2022-04-11 17:38:09 -0500

    preparando para counter

--- src/components/CardTodo.js
+++ src/components/CardTodo.js
@@ -1,5 +1,5 @@
 import { Card, Button } from 'antd'
-import { Divider, Avatar, Image, Row, Col } from 'antd';
+import { Divider, Avatar, Image, Row, Col, Badge } from 'antd';
 
 const CardTodo = ({index, task, deleteuser}) => {
   return (
@@ -7,7 +7,9 @@ const CardTodo = ({index, task, deleteuser}) => {
       <Card size="small" title="Todo task" style={{ width: 300 }}>
         <Row>
           <Col span={12}>
+          <Badge count={1}>
             <Avatar size={64} src={<Image src="https://aiptcomics.com/wp-content/uploads/2019/12/BATMAN_Cv86-1.jpg" style={{ width: 70 }} />} />
+          </Badge>
           </Col>
           <Col span={12}>
             <h3>{task.task}</h3>
