89c224e79f863955cd767768fdeff982caf46049
Francisco Rangel <francisco.rangel@s2g.energy>
2022-04-12 12:02:33 -0500

    agregando un counter para mostrar como notificacion en el avatar

--- src/components/CardTodo.js
+++ src/components/CardTodo.js
@@ -1,18 +1,33 @@
-import { Card, Button } from 'antd'
-import { Divider, Avatar, Image, Row, Col, Badge } from 'antd';
+import { useState } from 'react';
+import { Divider,
+  Avatar,
+  Image,
+  Row,
+  Col,
+  Badge,
+  Card,
+  Button } from 'antd';
+
 
 const CardTodo = ({index, task, deleteuser}) => {
+  const [count, setCount] = useState(0)
+
+  const useCount = () => {
+    setCount(count + 1)
+  }
+
   return (
     <div>
       <Card size="small" title="Todo task" style={{ width: 300 }}>
         <Row>
           <Col span={12}>
-          <Badge count={1}>
+          <Badge count={count}>
             <Avatar size={64} src={<Image src="https://aiptcomics.com/wp-content/uploads/2019/12/BATMAN_Cv86-1.jpg" style={{ width: 70 }} />} />
           </Badge>
           </Col>
           <Col span={12}>
             <h3>{task.task}</h3>
+            <Button type="primary" onClick={useCount}>Add</Button>
           </Col>
         </Row>
         <Divider/>
